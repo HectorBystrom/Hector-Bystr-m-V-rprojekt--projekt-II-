3e819b9ebae40c49c012edabee4c873557286b4a
Hector Byström <bystrom.hector@elev.ga.ntig.se>
2022-04-03 18:19:21 +0200

    Mycket ändringar + js gjort, slutprodukt

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 "use strict"; //använd modern javascript
 
-let todos = [];
+const LOCAL_STORAGE_KEY_TODOS = "app.todos";
+const LOCAL_STORAGE_KEY_TODOS_DATE = "app.todosDate";
+
+
+let todos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_TODOS)) || [];
+let todosDate = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_TODOS_DATE)) || [];
+
+
 let listRoot = document.querySelector("#list-root");
 let listForm = document.querySelector("[data-list-form]");
+let listFormDate = document.querySelector("[data-list-form-date]")
 let listInput = document.querySelector("[data-list-input]");
+let listInputDate = document.querySelector("[data-list-input-date]");
+
 
 listForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -13,6 +23,16 @@ listForm.addEventListener("submit", (e) => {
   todos.push(listInput.value.trim());
   updateList();
   listInput.value = "";
+});
+
+listFormDate.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (listInputDate.value.trim() === "") {
+    return;
+  }
+  todosDate.push(listInputDate.value.trim());
+  updateList();
+  listInputDate.value = "";
 });
 
 function todoList(items) {
@@ -30,13 +50,21 @@ function todoList(items) {
 function removeItem(event) {
   let itemToRemove = event.target.innerText;
   todos = todos.filter((item) => item !== itemToRemove);
+  todosDate = todos.filter((item) => item !== itemToRemove);
   updateList();
 }
 
 function updateList() {
+  saveList();
   listRoot.innerHTML = "";
   listRoot.append(todoList(todos));
+  listRoot.append(todoList(todosDate));
+}
+
+
+function saveList() {
+  localStorage.setItem(LOCAL_STORAGE_KEY_TODOS, JSON.stringify(todos));
+  localStorage.setItem(LOCAL_STORAGE_KEY_TODOS_DATE, JSON.stringify(todosDate));
 }
 
 updateList();
-    
